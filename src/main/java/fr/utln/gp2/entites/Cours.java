ec7715792c57e316d10c182db948e33b9a69d50d
Jeanne Barastier <jeanne.barastier@icloud.com>
2025-03-19 16:40:27 +0100

    modif cours

--- src/main/java/fr/utln/gp2/entites/Cours.java
+++ src/main/java/fr/utln/gp2/entites/Cours.java
@@ -14,6 +14,26 @@ public class Cours {
     @Id
     private UUID id;
 
+    //private UE ue;
+
     @ManyToMany(mappedBy = "cours")
     private List<Promotion> promos;
+
+    @ManyToMany
+    private List<Personne> intervenants;
+
+    //private Salle salle;
+
+    private float heureDebut;
+
+    private float duree;
+
+    private Date jour;
+
+    private enum Type {
+        CM,
+        TD,
+        TP
+    }
+    private Type type;
 }
