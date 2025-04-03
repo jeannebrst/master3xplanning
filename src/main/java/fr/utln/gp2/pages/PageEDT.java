fbc921364286bc189a2fb48301006ae5899cf131
user <quentin.ffa@gmail.com>
2025-04-03 15:40:19 +0200

    Premier pas de pageModif

--- src/main/java/fr/utln/gp2/pages/PageEDT.java
+++ src/main/java/fr/utln/gp2/pages/PageEDT.java
@@ -258,13 +258,11 @@ public class PageEDT {
 				int num = i*6 + j;
 				StackPane cell = new StackPane();
 				cell.setBackground(new Background(new BackgroundFill(Color.BLUE, new CornerRadii(0), new Insets(1))));
-				cell.setOnMouseClicked(event -> {
-					Platform.runLater(()-> {
-					PageModif pageModif = new PageModif(p);
-					pageModif.show();}
+				cell.setOnMouseClicked(event -> 
+					ouvrirPageModif()
 					);
 					
-				});
+				;
 				grilleEdt.add(cell, j, i);
 			}
 		}
@@ -313,6 +311,7 @@ public class PageEDT {
 		// Crée un StackPane pour centrer le label dans la cellule
 		StackPane cell = new StackPane();
 		cell.getChildren().add(boiteVbox);
+		cell.setOnMouseClicked(event->ouvrirPageModif());
 		//cell.getChildren().add(label);
 		cell.setAlignment(Pos.CENTER);  
 		cell.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);  
@@ -421,4 +420,14 @@ public class PageEDT {
 		return boutonModif;
 	}
 
+	public void ModifierLesCellules(){
+
+	}
+	private void ouvrirPageModif(){
+		Platform.runLater(()-> {
+			PageModif pageModif = new PageModif(p);
+			pageModif.show();
+		});
+		
+	}
 }
